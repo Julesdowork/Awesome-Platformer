06d70b40d0d2a5cb74d31c7dd25987d92951a3b6
Julian McNeill <julesdowork@gmail.com>
2019-10-21 23:20:11 -0400

    Snail Enemy

---
+++ Awesome Platformer/Assets/Scripts/Enemies/Snail.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snail : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    public Transform downCollision;
+
+    private bool moveLeft;
+    
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveLeft = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveLeft)
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+        else
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+
+        CheckCollision();
+    }
+
+    private void CheckCollision()
+    {
+        // if we don't detect collision anymore, do this...
+        if (!Physics2D.Raycast(downCollision.position, Vector2.down, 0.1f))
+            ChangeDirection();
+    }
+
+    private void ChangeDirection()
+    {
+        moveLeft = !moveLeft;
+
+        Vector3 tempScale = transform.localScale;
+
+        if (moveLeft)
+            tempScale.x = Mathf.Abs(tempScale.x);
+        else
+            tempScale.x = -Mathf.Abs(tempScale.x);
+
+        transform.localScale = tempScale;
+    }
+}
