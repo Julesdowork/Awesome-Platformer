487eeadc331639929054566fd958b56c2719942a
Julian McNeill <julesdowork@gmail.com>
2019-10-24 21:27:20 -0400

    Score Script

---
+++ Awesome Platformer/Assets/Scripts/Player/Score.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private int scoreCount;
+
+    private Text coinText;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinText = GameObject.Find("Coins Text").GetComponent<Text>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.CompareTag(MyTags.COIN_TAG))
+        {
+            target.gameObject.SetActive(false);
+            scoreCount++;
+            coinText.text = "x" + scoreCount;
+            audioSource.Play();
+        }
+    }
+}
